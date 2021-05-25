e37d2efa465fa904b6419840253b9632d5b45aae
Huiluo Cao <huiluocao@gmail.com>
2021-05-25 14:28:01 +0800

    Update sankey.R

--- sankey_alluvial/sankey.R
+++ sankey_alluvial/sankey.R
@@ -2,5 +2,8 @@ R官网：https://cran.r-project.org/web/packages/riverplot/index.html
 riverplot作者博客：https://logfc.wordpress.com/2014/02/27/riverplot/
 Plotting Flows with riverplot
 http://www.exegetic.biz/blog/2014/08/plotting-flows-with-riverplot/
+
+https://www.r-graph-gallery.com/321-introduction-to-interactive-sankey-diagram-2.html
+
 使用R画桑基图(流程图) http://blog.csdn.net/xwydq/article/details/44962277
 桑基图（sankey）的绘制 http://blog.csdn.net/qq_15167261/article/details/49736241
